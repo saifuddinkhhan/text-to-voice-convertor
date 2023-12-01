c91601f66d80e275a50093e5abfd44d4db581a61
saifuddinkhhan <137624775+saifuddinkhhan@users.noreply.github.com>
2023-12-01 20:30:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ speech.voice = voices[0];
 voices.forEach((voice,i)=>(voiceSelect.options[i]=new Option(voice.name,i)));
 };
 voiceSelect.addEventListener("change",()=>{
-    speech.voice = voice[voiceSelect.value];
+    speech.voice = voices[voiceSelect.value];
 });
 
 
